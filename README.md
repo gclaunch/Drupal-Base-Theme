cfc59acd341849fa68fc1a0f1f199388debc063b
gclaunch <claunch.grant@gmail.com>
2022-01-12 10:32:30 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+## Installation ##
+
+1. Inside theme directory `cd ~/www/web/themes/basestart`
+
+2. 
---
+++ src/js/functionality.js
@@ -0,0 +1,32 @@
+(function ($, Drupal, drupalSettings, window, document, undefined) {
+
+  /********************
+   ** Global Variables
+   ********************/
+
+
+  
+  /********************
+   ** jQuery DOM Ready
+   ********************/
+  jQuery(document).ready(function () {
+  });
+
+  /********************
+   ** jQuery PAGE Ready
+   ********************/
+  jQuery(window).on( "load", function () {
+  });
+
+  /********************
+   ** jQuery AJAX Ready
+   ********************/
+  jQuery(document).ajaxSuccess(function () {
+  });
+
+  //Drupal behaviors. To understand behaviors, see https://drupal.org/node/756722#behaviors
+  Drupal.behaviors.basestart_behavior = {
+
+  };
+
+})(jQuery, Drupal, drupalSettings, this, this.document);
